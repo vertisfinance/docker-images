c0362da03759ecf9eb1bcebd279270596cb57945
Richard Bann <richard.bann@vertis.com>
2016-03-17 12:25:01 +0100

    buildall modified

--- buildall.sh
+++ buildall.sh
@@ -3,13 +3,14 @@
 set -e
 
 dir="$(dirname "$BASH_SOURCE")"
+basename=vertisfinance
 
 for name in base postgres django-py3 nginx nodejs
 do
-    out="vertisfinance/${name}"
+    out="${basename}/${name}"
     outlen=${#out}
     line=$(head -c ${outlen} < /dev/zero | tr '\0' '-')
     echo $(tput setaf 3)${out}$(tput sgr0)
     echo $(tput setaf 2)${line}$(tput sgr0)
-    docker build -t "vertisfinance/$name" "$dir/$name/context"
+    docker build -t ${out} ${dir}/${name}/context
 done
